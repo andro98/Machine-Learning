cfa8c569616aa0fb4aba9c1a9026f31f41de8e51
Andrew Maher <andrewaacodo12@gmail.com>
2019-10-18 19:01:15 +0200

    Linear Regression Salary data

---
+++ Regression/Simple Linear Regression/Salary/salary_linear_regression.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 18 18:36:55 2019
+
+@author: andro
+"""
+
+#Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Import Data set
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,1].values
+
+#Splitting the dataset into the training and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 1/3, random_state = 0)
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+# Visualizing the training set results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.show()
+
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_test, y_pred, color = 'blue')
+plt.show()
